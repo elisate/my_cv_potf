746c3ad728717646c3df913f32fdbfa52b9d9eda
elisate <dushimiyimanaelisa@gmail.com>
2024-04-03 22:11:00 +0200

    adding delete functionality

--- src/Dashboard/Contactget.jsx
+++ src/Dashboard/Contactget.jsx
@@ -3,11 +3,12 @@ import axios from "axios";
 import "./Contactget.scss";
 import { MdDeleteForever } from "react-icons/md";
 import ReactPaginate from "react-paginate";
-
+import Notiflix from "notiflix";
 function Contactget() {
   const [contact, setContact] = useState([]);
   const [currentPage, setCurrentPage] = useState(0); // Start from page 1, so currentPage should start from 0
   const [itemsPerPage] = useState(5);
+
 
   useEffect(() => {
     const fetchdata = async () => {
@@ -38,7 +39,44 @@ function Contactget() {
     const selectedPage = data.selected;
     setCurrentPage(selectedPage);
   };
+ const [deleteC,setDeleteC]=useState(false);
+ const[contactdel,setContactdel]=useState(null);
+  const handledelete= async (id) =>
+  {
+  try{
+    Notiflix.Confirm.show(
+      "confirm delete contact",
+      "do you want to delete?",
+      "Yes",
+      "No",
+      async ()=>{
+     
+         const res = await axios.delete(
+           `https://api-potf.onrender.com/deletecontact/${id}`
+         );
+         window.location.reload();
 
+      },
+      ()=>{
+        alert("If you say so...")
+      },
+      {}
+    );
+  }
+  catch(error){
+console.log(error);
+  }
+  }
+
+// const handleClickdelete=(contact)=>
+//   {
+// setContactdel(contact);
+// handledelete();
+//   }
+  const handleCancel=()=>
+  {
+    setDeleteC(false);
+  }
   return (
     <div className="contact-holder-dash">
       <div className="cont-dash">
@@ -62,10 +100,20 @@ function Contactget() {
               <td>{item.subject}</td>
               <td>{item.message}</td>
               <td>
-                <MdDeleteForever className="delete-contact-icon" />
+                <MdDeleteForever
+                  className="delete-contact-icon"
+                  onClick={() => handledelete(item._id)}
+                />
               </td>
             </tr>
           ))}
+          {deleteC && (
+            <div>
+              <p>Are you sure to delete {contactdel._id}</p>
+              <button onClick={handledelete}>Ok</button>
+              <button onClick={handleCancel}>Cancel</button>
+            </div>
+          )}
         </tbody>
       </table>
       <div className="pagination">
